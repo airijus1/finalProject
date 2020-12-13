fde570f73579eeeff8b6fbaef7f1d8cef8801781
airijus1 <46110821+airijus1@users.noreply.github.com>
2020-12-13 21:51:33 +0000

    Update config.php

--- finalProject/config.php
+++ finalProject/config.php
@@ -1,9 +1,9 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_SERVER', 'eu-cdbr-west-03.cleardb.net');
+define('DB_USERNAME', 'be8d2047567ed9');
+define('DB_PASSWORD', '800f7dca');
 define('DB_NAME', 'demo');
  
 /* Attempt to connect to MySQL database */
